4df904dcfa2cfb8f8bfdd60355b8926925f28fe0
Carter Ash <36821628+Cash96@users.noreply.github.com>
2020-07-28 14:48:39 -0500

    Add files via upload

---
+++ Button_Buzzer_Music.py
@@ -0,0 +1,53 @@
+#Import tools
+import RPi.GPIO as GPIO    #control the GPIO pins with code
+import BMC                 #custom class file to control button matrix
+
+#watch for warnings
+GPIO.setwarnings(False)
+
+#Set up buzzer pin
+buzzer_pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buzzer_pin, GPIO.OUT)
+
+#set up PWM
+DutyCycle = 1               #Duty cycle can be 1-99 in value
+p = GPIO.PWM(buzzer_pin, 1)
+
+#create a button matrix object
+buttons = BMC.ButtonMatrix()
+
+#A function to change the duty cycle and frequency based on the button number
+#B = the button being pressed
+#human frequency for hearing goes from 20-20,000
+def ChangeSound(B):    
+    if B == 0:
+        print("Changing frequency to 5")
+        p.ChangeFrequency(300)
+        DutyCycle = 5
+    elif B == 1:
+        print("Changing frequency to 90")
+        p.ChangeFrequency(400)
+    elif B == 2:
+        print("Changing frequency to 90")
+        p.ChangeFrequency(500)
+    #add more elif statements here to accomodate more buttons
+        
+try:
+    while(True):
+        for j in range(len(buttons.columnPins)):
+            # set each output pin to low
+            GPIO.output(buttons.columnPins[j],0)
+            for i in range(len(buttons.rowPins)):
+                if GPIO.input(buttons.rowPins[i]) == 0:
+                    # button pressed, activate it store the value
+                    btn = buttons.activateButton(i,j)  #the number button being pressed
+                    ChangeSound(btn)
+                    p.start(DutyCycle)
+                    while buttons.buttonHeldDown(i):
+                        p.start(DutyCycle)
+                    p.stop()
+            # return each output pin to high
+            GPIO.output(buttons.columnPins[j],1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
